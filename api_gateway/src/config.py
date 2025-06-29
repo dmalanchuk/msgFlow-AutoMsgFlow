dc28ddc3c89d434d4f99743514708dcb8945bae2
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-06-29 13:44:43 +0300

    update conf file

--- api_gateway/src/config.py
+++ api_gateway/src/config.py
@@ -2,7 +2,16 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    """Application settings: name, host, port"""
     SERVICE_NAME: str = "API Gateway"
+    SERVICE_HOST: str = "0.0.0.0"
+    SERVICE_PORT: int = 8000
+
+    """All url for services"""
+    AUTH_SERVICE_URL: str = "http://auth_service:8000"
+
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()
