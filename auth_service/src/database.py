4b4fe2aac10fcbb23aff1a7607ff75e65e9106b8
M1Hato <miro28795@gmail.com>
2025-07-01 00:19:15 +0300

    create code database url

---
+++ auth_service/src/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+new_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+Base = declarative_base()
+
+async def get_session():
+    async with new_session() as session:
+        yield session
