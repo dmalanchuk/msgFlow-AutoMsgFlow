246e47c21a286f63dd4b9c90655492e599962a2d
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-02 16:16:47 +0300

    connections

--- integration_service/src/main.py
+++ integration_service/src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.connections.telegram.telegram_conn import router as telegram_router
+from src.connections.telegram.telegram_integration import router as telegram_router
 
 app = FastAPI()
 app.include_router(telegram_router, tags=["telegram"])
