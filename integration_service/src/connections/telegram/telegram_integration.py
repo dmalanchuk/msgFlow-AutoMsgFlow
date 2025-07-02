3cb8e565212e7beb1a1f59573ad895dc332320a5
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-02 16:16:47 +0300

    connections

--- integration_service/src/connections/telegram/telegram_integration.py
+++ integration_service/src/connections/telegram/telegram_integration.py
@@ -1,12 +1,18 @@
 from fastapi import Request
 
 from faststream.rabbit.fastapi import RabbitRouter
+from src.services.event_builder import EventBuilder
 
 router = RabbitRouter()
 
 
 @router.post("/webhook/telegram")
-async def order(request: Request):
+async def telegram_webhook(request: Request):
     body = await request.json()
+    event = EventBuilder.build_event_from_telegram(body)
 
-    return {"status": "ok"}
+    if event:
+        await router.broker.publish(event, queue="incoming.events")
+        return {"status": f"Event {event['event_type']} sent"}
+
+    return {"status": "Unsupported event"}
