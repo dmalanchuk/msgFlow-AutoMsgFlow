ca7c088146dbe6021329f92526a04bb444cfd57e
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-04 14:28:27 +0300

    change schema on model in scenario repo

--- workflow_service/src/repositories/scenario_repo.py
+++ workflow_service/src/repositories/scenario_repo.py
@@ -1,12 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.schemas.scenario_schema import ScenarioCreate
 from src.models.scenarios_model import ScenariosModel
 
 
 class ScenarioRepo:
 
     @staticmethod
-    async def create_scenario(session: AsyncSession, scenario: ScenarioCreate):
+    async def create_scenario(session: AsyncSession, scenario: ScenariosModel):
         session.add(scenario)
 
         await session.commit()
