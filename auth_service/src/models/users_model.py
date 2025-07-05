e9afd678fae8d327c7716235cb0e71ee6730a9d4
M1Hato <miro28795@gmail.com>
2025-07-05 12:57:03 +0300

    add relationship with LoginTokens

--- auth_service/src/models/users_model.py
+++ auth_service/src/models/users_model.py
@@ -1,5 +1,7 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database import Base
+from src.models.login_tokens_model import LoginTokens
+
 
 class UserModel(Base):
     __tablename__ = "users"
@@ -8,3 +10,5 @@ class UserModel(Base):
     email: Mapped[str] = mapped_column(nullable=False)
     username: Mapped[str] = mapped_column(nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+
+    tokens: Mapped[list["LoginTokens"]] = relationship(back_populates="user")
