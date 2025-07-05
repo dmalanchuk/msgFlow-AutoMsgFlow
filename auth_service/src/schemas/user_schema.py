0dc8e38e9e3eb9db529e7d14253bf7f78aba9448
M1Hato <miro28795@gmail.com>
2025-07-05 12:55:27 +0300

    add LoginUser

--- auth_service/src/schemas/user_schema.py
+++ auth_service/src/schemas/user_schema.py
@@ -8,3 +8,8 @@ class User(BaseModel):
 
 class CreateUser(User):
     password: str = Field(min_length=8)
+
+
+class LoginUser(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8)
