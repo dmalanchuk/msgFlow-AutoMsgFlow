a3e76ff630046660402201bf31e2300a122d1bda
M1Hato <miro28795@gmail.com>
2025-07-05 22:29:22 +0300

    add api logout user

--- auth_service/src/api/auth_api.py
+++ auth_service/src/api/auth_api.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends, Body
+from fastapi import APIRouter, Depends, Body, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
 
+from starlette.requests import Request
 from starlette.responses import Response
 
+from src.repositories.refresh_tokens_repo import RefreshTokenRepo
 from src.schemas.user_schema import CreateUser, LoginUser
 from src.database import get_session
 from src.services.login_service import LoginService
@@ -27,3 +29,13 @@ async def login_user(
     session: AsyncSession = Depends(get_session)
 ):
     return await LoginService.login_user_service(data, response, session)
+
+@auth_router.post("/logout")
+async def logout_user(request: Request, response: Response, session: AsyncSession = Depends(get_session)):
+    refresh_token = request.cookies.get("refresh_token")
+
+    if not refresh_token:
+        raise HTTPException(status_code=401, detail="Refresh token required")
+
+    await RefreshTokenRepo.deactivate_refresh_token(refresh_token, response, session)
+    return {"detail": "Successfully logout"}
