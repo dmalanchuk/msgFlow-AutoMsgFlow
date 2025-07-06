cd22ae81e1fa1fb7a44137020861237587216996
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-06 16:28:22 +0300

    added and del lifespan

--- integration_service/src/main.py
+++ integration_service/src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
 from src.routes.router import router
 
-app = FastAPI(title="Integration Service")
+app = FastAPI(title="Integration Service", )
 app.include_router(router)
