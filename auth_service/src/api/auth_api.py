a87729ab3a9eb5601cc9a84e5cd83ad04b1001ec
M1Hato <miro28795@gmail.com>
2025-07-10 01:16:37 +0300

    done task team lead

--- auth_service/src/api/auth_api.py
+++ auth_service/src/api/auth_api.py
@@ -1,17 +1,15 @@
 from fastapi import APIRouter, Depends, Body, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-
-
 from starlette.requests import Request
 from starlette.responses import Response
-
 from src.repositories.refresh_tokens_repo import RefreshTokenRepo
-from src.repositories.user_repo import UserRepo
 from src.schemas.user_schema import CreateUser, LoginUser
 from src.database import get_session
+from src.services.get_profile_service import GetProfileService
 from src.services.login_service import LoginService
+from src.services.logout_service import LogoutService
 from src.services.reg_service import RegServices
-from src.models.users_model import UserModel
+
 
 auth_router = APIRouter(
     prefix="/auth",
@@ -35,31 +33,12 @@ async def login_user(
 
 @auth_router.post("/logout")
 async def logout_user(request: Request, response: Response, session: AsyncSession = Depends(get_session)):
-    refresh_token = request.cookies.get("refresh_token")
-
-    if not refresh_token:
-        raise HTTPException(status_code=401, detail="Refresh token required")
-
-    await RefreshTokenRepo.deactivate_refresh_token(refresh_token, response, session)
-    return {"detail": "Successfully logout"}
+    return await LogoutService.logout_service(request, response, session)
 
 @auth_router.get("/profile")
 async def get_profile(
     request: Request,
     session: AsyncSession = Depends(get_session)
 ):
-    user_email = request.headers.get("X-User-Email")
-    print("🧾 Отриманий X-User-Email:", user_email)
-    if not user_email:
-        raise HTTPException(status_code=400, detail="Missing user email in request state")
+    return await GetProfileService.get_profile_service(request, session)
 
-    # Знайти користувача по email
-    user: UserModel | None = await UserRepo.get_by_email(session, user_email)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    return {
-        "id": user.id,
-        "email": user.email,
-        "username": user.username
-    }
---
+++ auth_service/src/services/get_profile_service.py
@@ -0,0 +1,25 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.requests import Request
+from src.repositories.user_repo import UserRepo
+from src.models.users_model import UserModel
+
+class GetProfileService():
+    @staticmethod
+    async def get_profile_service(request: Request, session: AsyncSession):
+
+        user_email = request.headers.get("X-User-Email")
+        print("🧾 Отриманий X-User-Email:", user_email)
+        if not user_email:
+            raise HTTPException(status_code=400, detail="Missing user email in request state")
+
+        # Знайти користувача по email
+        user: UserModel | None = await UserRepo.get_by_email(session, user_email)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        return {
+            "id": user.id,
+            "email": user.email,
+            "username": user.username
+        }
