e88f80d7282de9563329efeea05058d0d38da80e
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-11 14:37:37 +0300

    added lifespan

--- workflow_service/src/main.py
+++ workflow_service/src/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
-
+from contextlib import asynccontextmanager
 from src.api.scenario_api import router
+from src.rabbitmq.subscriber import broker
 
-app = FastAPI()
+""" FastAPI app with RabbitMQ subscriber """
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    await broker.connect()
+    yield
+    await broker.close()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(router, tags=["scenarios"])
