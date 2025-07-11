3a917d5a5711f91606c63dc5bdedd10da0a1ce2c
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-11 14:37:49 +0300

    create listener

--- workflow_service/src/rabbitmq/subscriber.py
+++ workflow_service/src/rabbitmq/subscriber.py
@@ -1,4 +1,9 @@
+from faststream.rabbit import RabbitBroker
+from src.config import settings
+
+broker = RabbitBroker(settings.RABBITMQ_URL)
 
 
+@broker.subscriber(settings.QUEUE_NAME)
 async def subscriber(channel):
-    await channel.start_consuming()
+    ...
