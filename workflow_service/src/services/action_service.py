1d840c42e4a9dc56092e05fa403d4c57dd70592d
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-16 14:10:30 +0300

    starting write logic action service

--- workflow_service/src/services/action_service.py
+++ workflow_service/src/services/action_service.py
@@ -1,2 +1,8 @@
+"""
+    Create an action service on trigger words that will be sent to the broker and sent to the
+    integration service, from there they will call functions
+"""
+
+
 class ActionService:
     ...
