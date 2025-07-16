81eae68de0f8cfaa8e57a0f3851a16b4a67ea644
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-16 14:22:24 +0300

    wrote publish action for integration service

--- workflow_service/src/rabbitmq/publish_action.py
+++ workflow_service/src/rabbitmq/publish_action.py
@@ -1,1 +1,8 @@
 from faststream.rabbit import RabbitBroker
+from src.config import settings
+
+broker = RabbitBroker(settings.RABBITMQ_URL)
+
+
+async def publish_action(action: dict):
+    await broker.publish(action, settings.ACTION_QUEUE_NAME)
