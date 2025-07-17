b49bf482497d6ab63e86fe22a5d231afb0ed52cf
M1Hato <miro28795@gmail.com>
2025-07-18 02:09:42 +0300

    added logic user_email

--- workflow_service/src/services/scenario_service.py
+++ workflow_service/src/services/scenario_service.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException
+from fastapi import Request, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.models.scenarios_model import ScenariosModel
@@ -6,6 +6,7 @@ from src.schemas.scenario_schema import ScenarioCreate
 
 from src.repositories.scenario_repo import ScenarioRepo
 from src.services.get_chat_id_service import GetChatIdService
+from src.services.scenario_get_email_service import ScenarioGetEmailService
 
 """
     Business logic service for processing, creating, 
@@ -16,16 +17,17 @@ from src.services.get_chat_id_service import GetChatIdService
 class ScenarioService:
 
     @staticmethod
-    async def create_scenario(session: AsyncSession, scenario: ScenarioCreate, owner_email: str):
+    async def create_scenario(session: AsyncSession, scenario: ScenarioCreate, request: Request):
 
         try:
             chat_id = await GetChatIdService.get_chat_id(scenario.chat_url)
+            user_email = ScenarioGetEmailService.get_user_email(request)
         except ValueError as e:
             raise HTTPException(status_code=400, detail=str(e))
 
         new_scenario = ScenariosModel(
             name=scenario.name,
-            owner_email=owner_email,
+            owner_email=user_email,
             chat_id=chat_id,
             event=scenario.event.model_dump(),
             conditions=scenario.condition.model_dump(),
