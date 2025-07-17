51df298fa0d50b055f283d9f7eb3f98d9a412d1f
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-18 00:33:56 +0300

    added new func publish actions and added processing logic saving msg in redis

--- workflow_service/src/rabbitmq/subscriber.py
+++ workflow_service/src/rabbitmq/subscriber.py
@@ -7,9 +7,17 @@ broker = RabbitBroker(settings.RABBITMQ_URL)
 
 
 @broker.subscriber(settings.QUEUE_NAME)
-async def subscriber(message: dict):
-    chat_id = message["chat_id"]
-    text = message["text"]
+async def handle_incoming_message(message: dict):
+    try:
+        chat_id = message["chat_id"]
+        text = message["text"]
 
-    await ServiceRedis.save_message(chat_id, text)
-    print(f"Message saved in Redis for chat: {chat_id}")
+        await ServiceRedis.save_message(chat_id, text)
+        print(f"Message saved in Redis for chat: {chat_id}")
+
+    except Exception as e:
+        print(f"Error saving message in Redis: {e}")
+
+
+async def publish_action(action: dict):
+    await broker.publish(action, settings.ACTION_QUEUE_NAME)
