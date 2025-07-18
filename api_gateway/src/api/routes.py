26cc7df85187285ed94e763ba278406692faa832
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-18 15:42:44 +0300

    added comments and del unnecessary proxy url

--- api_gateway/src/api/routes.py
+++ api_gateway/src/api/routes.py
@@ -6,7 +6,11 @@ from src.utils.proxy_utils import proxy_request
 
 router = APIRouter()
 
-"""proxy to services"""
+"""
+    auth service service have one get requests and three post request
+    get request: /profile
+    post request: /login, /register, /logout
+"""
 
 
 @router.get("/auth/{path:path}")
@@ -17,29 +21,32 @@ async def auth_proxy_get(path: str, request: Request):
 
 @router.post("/auth/{path:path}")
 async def auth_proxy_with_body(
-    path: str,
-    request: Request,
-    json_body: Optional[dict] = Body(default=None)
+        path: str,
+        request: Request,
+        json_body: Optional[dict] = Body(default=None)
 ):
     url = f"{settings.AUTH_SERVICE_URL}/auth/{path}"
     return await proxy_request(request, url)
+
+
+"""
+    workflow service have two get requests and one post request
+    get request: /metadata/actions, /metadata/conditions
+    post request: /create
+"""
+
 
 @router.get("/scenarios/{path:path}")
 async def scenarios_proxy_get(path: str, request: Request):
     url = f"{settings.WORKFLOW_SERVICE_URL}/scenarios/{path}"
     return await proxy_request(request, url)
 
+
 @router.post("/scenarios/{path:path}")
 async def scenarios_proxy_post(
-    path: str,
-    request: Request,
-    json_body: Optional[dict] = Body(default=None)
+        path: str,
+        request: Request,
+        json_body: Optional[dict] = Body(default=None)
 ):
     url = f"{settings.WORKFLOW_SERVICE_URL}/scenarios/{path}"
-    return await proxy_request(request, url)
-
-#integrations
-@router.api_route("/integration{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
-async def auth_proxy(path: str, request: Request):
-    url = f"{settings.INTERACTION_SERVICE_URL}/{path}"
     return await proxy_request(request, url)
