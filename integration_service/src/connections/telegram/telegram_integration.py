4d4381abf9fab6a6684b40527b16a48840a2b236
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-21 18:02:45 +0300

    updated telegram webhook now his getting all info form telegram, all event

--- integration_service/src/connections/telegram/telegram_integration.py
+++ integration_service/src/connections/telegram/telegram_integration.py
@@ -1,31 +1,48 @@
 from fastapi import Request, APIRouter
+from typing import Any
 
 from src.rabbitmq.publisher import publish_to_queue
+from src.services.telegram_service import TelegramService
 
 router = APIRouter()
 
 
 @router.post("/telegram")
 async def telegram_webhook(request: Request):
-    body = await request.json()
+    body: dict[str, Any] = await request.json()
+    update_type = await TelegramService.get_update_type(body)
 
-    message = body.get("message")
+    chat_id = await TelegramService.extract_chat_id(body, update_type)
+    username = await TelegramService.extract_username(body, update_type)
 
-    if not message:
-        return {"status": "no message"}
+    payload = {
+        "source": "telegram",
+        "update_type": update_type,
+        "chat_id": chat_id,
+        "username": username,
+        "raw": body
+    }
 
-    chat = message.get("chat", {})
-    from_user = message.get("from", {})
-    text = message.get("text")
+    # додаткові поля для конкретних типів
+    if update_type == "message":
+        payload["text"] = body["message"].get("text")
 
-    if text:
-        payload = {
-            "text": text,
-            "chat_id": chat.get("id"),
-            "username": from_user.get("username"),
-            "source": "telegram"
-        }
-        await publish_to_queue(payload)
-        return {"status": "ok"}
-    else:
-        return {"status": "ignored"}
+    elif update_type == "callback_query":
+        payload["text"] = body["callback_query"].get("data")
+
+    elif update_type == "inline_query":
+        payload["query"] = body["inline_query"].get("query")
+
+    elif update_type == "poll_answer":
+        payload["poll_id"] = body["poll_answer"].get("poll_id")
+        payload["answers"] = body["poll_answer"].get("option_ids")
+
+    elif update_type == "chat_member":
+        payload["new_status"] = body["chat_member"]["new_chat_member"].get("status")
+
+    elif update_type == "chat_join_request":
+        payload["user"] = body["chat_join_request"]["from"]
+
+    await publish_to_queue(payload)
+    return {"status": "ok"}
+
