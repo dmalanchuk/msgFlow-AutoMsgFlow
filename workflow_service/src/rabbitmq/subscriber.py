50377c34576d33101991f2901d79bbc9fdae4282
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-21 14:19:05 +0300

    wrote new subscriber, but i have rewrite it

--- workflow_service/src/rabbitmq/subscriber.py
+++ workflow_service/src/rabbitmq/subscriber.py
@@ -6,14 +6,59 @@ from src.config import settings
 broker = RabbitBroker(settings.RABBITMQ_URL)
 
 
-@broker.subscriber(settings.QUEUE_NAME)
-async def handle_incoming_message(message: dict):
-    try:
-        chat_id = message["chat_id"]
-        text = message["text"]
+# @broker.subscriber(settings.QUEUE_NAME)
+# async def handle_incoming_message(message: dict):
+#     try:
+#         chat_id = message["chat_id"]
+#         text = message["text"]
+#
+#         await ServiceRedis.save_update(chat_id, text)
+#         print(f"Message saved in Redis for chat: {chat_id}")
+#
+#     except Exception as e:
+#         print(f"Error saving message in Redis: {e}")
 
-        await ServiceRedis.save_message(chat_id, text)
-        print(f"Message saved in Redis for chat: {chat_id}")
+
+@broker.subscriber(settings.QUEUE_NAME)
+async def handle_incoming_message(update: dict):
+    try:
+        chat_id = None
+        update_type = "unknown"
+
+        for key in [
+            "message",
+            "edited_message",
+            "channel_post",
+            "edited_channel_post",
+            "inline_query",
+            "chosen_inline_result",
+            "callback_query",
+            "shipping_query",
+            "pre_checkout_query",
+            "poll",
+            "poll_answer",
+            "my_chat_member",
+            "chat_member",
+            "chat_join_request"
+        ]:
+            if key in update:
+                update_type = key
+                break
+
+        if update_type in ("message", "edited_message"):
+            chat_id = update[update_type]["chat"]["id"]
+        elif update_type in ("channel_post", "edited_channel_post"):
+            chat_id = update[update_type]["chat"]["id"]
+        elif update_type in ("my_chat_member", "chat_member", "chat_join_request"):
+            chat_id = update[update_type]["chat"]["id"]
+        elif update_type == "callback_query":
+            chat_id = update["callback_query"]["message"]["chat"]["id"]
+
+        if chat_id is not None:
+            await ServiceRedis.save_update(chat_id, update)
+            print(f"[{update_type}] saved for chat: {chat_id}")
+        else:
+            print(f"Не вдалося знайти chat_id для update_type: {update_type}")
 
     except Exception as e:
         print(f"Error saving message in Redis: {e}")
