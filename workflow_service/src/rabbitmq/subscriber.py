2558d9014024b0f344c12b94c580bb34e41a275b
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-22 18:27:57 +0300

    added three new args getting from message

--- workflow_service/src/rabbitmq/subscriber.py
+++ workflow_service/src/rabbitmq/subscriber.py
@@ -12,11 +12,18 @@ async def handle_incoming_message(message: dict):
         chat_id = message["chat_id"]
         raw_update = message["raw"]
         text = message.get("text")
+        msg_id = (
+                message.get("message_id") or
+                raw_update.get("edited_message", {}).get("message_id") or
+                raw_update.get("message", {}).get("message_id")
+        )
+        source = message["source"]
+        event_type = message["update_type"]
 
         await ServiceRedis.save_update(chat_id, raw_update)
 
         if text:
-            await ServiceRedis.save_message(chat_id, text)
+            await ServiceRedis.save_message(chat_id, text, msg_id, source, event_type)
 
         print(f"Update saved in Redis for chat: {chat_id}")
 
