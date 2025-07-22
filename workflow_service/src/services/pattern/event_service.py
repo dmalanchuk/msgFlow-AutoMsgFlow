7571d49d8c154d0dda37eb84f076c066c6acf60c
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-22 18:28:47 +0300

    wrote method getting source and event type

--- workflow_service/src/services/pattern/event_service.py
+++ workflow_service/src/services/pattern/event_service.py
@@ -1,10 +1,22 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from src.services.redis_service import ServiceRedis
 
 
 class EventService:
 
     @staticmethod
-    async def check_event(event_type: str, session: AsyncSession):
-        ...
+    async def check_event(chat_id: int):
+        updates = await ServiceRedis.get_last_messages(chat_id, limit=1)
+
+        if not updates:
+            return {"msg": "No updates"}
+
+        last_update = updates[0]
+        source = last_update["source"]
+        event_type = last_update["event_type"]
+
+        event_payload = {
+            "source": source,
+            "event_type": event_type
+        }
+
+        return event_payload
