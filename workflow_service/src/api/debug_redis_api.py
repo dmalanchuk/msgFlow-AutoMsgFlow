65419eb7f52d3a6f5c35c71efd474561a731be5c
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-22 18:27:13 +0300

    added new endpoint is get chat event

--- workflow_service/src/api/debug_redis_api.py
+++ workflow_service/src/api/debug_redis_api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from src.services.redis_service import ServiceRedis
+from src.services.pattern.event_service import EventService
 
 router_debug = APIRouter(prefix="/debug")
 
@@ -12,3 +13,8 @@ async def get_chat_messages(chat_id: int, limit: int = 10):
 @router_debug.get("/chat/{chat_id}/updates")
 async def get_chat_updates(chat_id: int, limit: int = 10):
     return await ServiceRedis.get_last_updates(chat_id, limit)
+
+
+@router_debug.get("/chat/{chat_id}/event")
+async def get_chat_event(chat_id: int):
+    return await EventService.check_event(chat_id)
