59fc3cd9f99828e5d6a246399039af7257fdbd58
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-22 15:43:12 +0300

    del old comments

--- workflow_service/src/services/redis_service.py
+++ workflow_service/src/services/redis_service.py
@@ -3,18 +3,21 @@ import json
 
 
 class ServiceRedis:
+    """saved last updates and messages"""
 
     @staticmethod
     async def save_update(chat_id: int, update: dict):
         key = f"chat:{chat_id}:updates"
         await redis.rpush(key, json.dumps(update))
-        await redis.expire(key, 86400)  # 1 день
+        await redis.expire(key, 500)  # 86400
 
     @staticmethod
     async def save_message(chat_id: int, text: str):
         key = f"chat:{chat_id}:messages"
         await redis.rpush(key, json.dumps({"text": text}))
-        await redis.expire(key, 86400)  # 1 день
+        await redis.expire(key, 500)  # 86400
+
+    """get last updates and messages"""
 
     @staticmethod
     async def get_last_messages(chat_id: int, limit: int = 1):
@@ -23,7 +26,7 @@ class ServiceRedis:
         return [json.loads(m) for m in messages]
 
     @staticmethod
-    async def get_last_updates(chat_id: int, limit: int = 10):
+    async def get_last_updates(chat_id: int, limit: int = 1):
         key = f"chat:{chat_id}:updates"
         updates = await redis.lrange(key, -limit, -1)
         return [json.loads(update) for update in updates]
