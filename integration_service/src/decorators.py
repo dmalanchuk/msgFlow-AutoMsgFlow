f46dcac977da4ba361de34b234bcd43086bd0873
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-27 11:59:19 +0300

    added first decorator timer

---
+++ integration_service/src/decorators.py
@@ -0,0 +1,15 @@
+import time
+from functools import wraps
+
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Function {func.__name__} took {end - start} seconds to run.")
+
+        return result
+
+    return wrapper
