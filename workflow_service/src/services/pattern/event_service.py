427a721724ec7523850e7b97a24608a89448e00b
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-27 15:24:06 +0300

    end write payload for save update (redis)

--- workflow_service/src/services/pattern/event_service.py
+++ workflow_service/src/services/pattern/event_service.py
@@ -5,18 +5,29 @@ class EventService:
 
     @staticmethod
     async def check_event(chat_id: int):
-        updates = await ServiceRedis.get_last_messages(chat_id, limit=1)
+        updates = await ServiceRedis.get_last_updates(chat_id, limit=1)
 
         if not updates:
             return {"msg": "No updates"}
 
         last_update = updates[0]
-        source = last_update["source"]
-        event_type = last_update["event_type"]
+        event_type = list(last_update)[1]
+        message_data = last_update.get("message") or last_update.get("edited_message")
+        chat_id = message_data.get(
+            "chat", {}).get(
+            "id", ""
+        )
+
+        if "new_chat_member" in message_data:
+            event_type = "new_chat_member"
+        elif "left_chat_member" in message_data:
+            event_type = "left_chat_member"
+        elif "photo" in message_data:
+            event_type = "photo"
 
         event_payload = {
-            "source": source,
-            "event_type": event_type
+            "event_type": event_type,
+            "chat_id": chat_id
         }
 
         return event_payload
