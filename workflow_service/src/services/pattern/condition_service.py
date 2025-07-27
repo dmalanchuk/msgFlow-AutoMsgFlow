56a65bcf2368393cf5f8a1f964c2d299f105e941
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-27 15:28:05 +0300

    starting write service conditions and added deco timer

--- workflow_service/src/services/pattern/condition_service.py
+++ workflow_service/src/services/pattern/condition_service.py
@@ -1,16 +1,13 @@
-from src.services.redis_service import ServiceRedis
 from src.schemas.condition_action_schema import ConditionAction
 from src.repositories.condition_action_repo import ConditionActionRepo
+from src.decorators import timer
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class ConditionService:
-    """
-        the text is taken from Redis and checked for a condition,
-        if it is found, the action is executed. Contains word condition.
-    """
 
     @staticmethod
-    async def receive_condition_from_scenario():
+    @timer
+    async def receive_condition():
         ...
