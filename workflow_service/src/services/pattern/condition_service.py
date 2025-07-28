9ce0043dae01061622e92b3017306ea32d19424c
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-28 22:28:40 +0300

    starting write new method check conditions

--- workflow_service/src/services/pattern/condition_service.py
+++ workflow_service/src/services/pattern/condition_service.py
@@ -7,12 +7,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 class ConditionService:
 
     @staticmethod
-    async def is_event_matched(chat_id: int, session: AsyncSession):
+    async def is_event_matched(chat_id: int, session: AsyncSession) -> bool:
         result = await ScenarioRepo.get_scenario(chat_id, session)
         event = await EventService.check_event(chat_id)
 
         if not event:
-            return {"msg": "no event right now"}
+            return False
 
         incoming_event_type = event.get("event_type")
         if not incoming_event_type:
@@ -24,3 +24,6 @@ class ConditionService:
 
         return False
 
+    @staticmethod
+    async def check_conditions(chat_id: int, session: AsyncSession) -> bool:
+        ...
