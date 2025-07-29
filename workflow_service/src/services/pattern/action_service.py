4039f854b89a8403a893177d12ee69c93288d4ec
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-29 23:26:50 +0300

    wrote send action to broker

--- workflow_service/src/services/pattern/action_service.py
+++ workflow_service/src/services/pattern/action_service.py
@@ -1,17 +1,22 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from schemas.condition_action_schema import ConditionAction
+from src.repositories.scenario_repo import ScenarioRepo
+from src.services.pattern.condition_service import ConditionService
 from src.rabbitmq.subscriber import publish_action
-from src.repositories.condition_action_repo import ConditionActionRepo
-
-"""
-    Create an action service on trigger words that will be sent to the broker and sent to the
-    integration service, from there they will call functions
-"""
 
 
 class ActionService:
 
     @staticmethod
-    async def send_action_from_scenario():
-        ...
+    async def send_action_from_scenario(chat_id: int, session: AsyncSession):
+        result: bool = await ConditionService.check_conditions(chat_id, session)
+        actions = await ScenarioRepo.get_scenario(chat_id, session)
+
+        if result == True:
+            for action in actions:
+                await publish_action(action.actions)
+                return {"msg": "Action sent", "action": action.actions}
+        else:
+            return {"msg": "conditions false"}
+
+        return None
