e052f5a234e9dd07d778c3a21f819012a94a8d0c
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-29 23:12:08 +0300

    wrote new method check conditions

--- workflow_service/src/services/pattern/condition_service.py
+++ workflow_service/src/services/pattern/condition_service.py
@@ -1,5 +1,6 @@
 from src.repositories.scenario_repo import ScenarioRepo
 from src.services.pattern.event_service import EventService
+from src.services.redis_service import ServiceRedis
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -26,4 +27,17 @@ class ConditionService:
 
     @staticmethod
     async def check_conditions(chat_id: int, session: AsyncSession) -> bool:
-        ...
+        conditions = await ScenarioRepo.get_scenario(chat_id, session)
+        result = await ConditionService.is_event_matched(chat_id, session)
+        text = await ServiceRedis.get_last_messages(chat_id)
+
+        last_message = text[0]
+
+        if result:
+            for condition in conditions:
+                if condition.conditions["params"].get("word") in last_message.get("text"):
+                    return True
+
+        return False
+
+
