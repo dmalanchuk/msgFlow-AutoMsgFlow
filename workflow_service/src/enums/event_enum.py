022c12c637c137e27875d55667e17d600fc69448
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-31 13:54:14 +0300

    update enum added two new types

--- workflow_service/src/enums/event_enum.py
+++ workflow_service/src/enums/event_enum.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 
 class EventType(str, Enum):
+    NEW_MESSAGE = "message"
+    EDITED_MESSAGE = "edited_message"
     NEW_MEMBER = "new_chat_member"
     LEFT_MEMBER = "left_chat_member"
     PHOTO = "photo"
