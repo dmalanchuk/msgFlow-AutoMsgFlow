ac1edfcaaf1a44adbd6c8c66d92c06e0479b7fe9
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-07-31 18:04:06 +0300

    del enums and update logic event service

--- workflow_service/src/services/pattern/event_service.py
+++ workflow_service/src/services/pattern/event_service.py
@@ -1,14 +1,13 @@
 from src.services.redis_service import ServiceRedis
 from src.logger import logger
 
-from src.enums.event_enum import EventType
 from src.schemas.event_schema import Event
 
 
 class EventService:
 
     @staticmethod
-    async def check_event(chat_id: int):
+    async def check_event(chat_id: int) -> dict:
         try:
             updates = await ServiceRedis.get_last_updates(chat_id, limit=1)
 
@@ -16,7 +15,6 @@ class EventService:
                 logger.info(f"No updates found for chat: {chat_id}")
                 return {"msg": "No updates"}
 
-            logger.debug(f"Last update found for chat: {chat_id}: {updates[0]}")
             return EventService._parse_update(updates[0])
 
         except Exception as e:
@@ -24,8 +22,9 @@ class EventService:
             return {"msg": "Internal error", "error": str(e)}
 
     @staticmethod
-    def _parse_update(update: dict):
+    def _parse_update(update: dict) -> dict:
         try:
+            event = list(update)[1]
             message = update.get("message") or update.get("edited_message") or {}
             chat_data = message.get("chat", {})
             chat_id = chat_data.get("id")
@@ -33,21 +32,24 @@ class EventService:
             if not chat_id:
                 raise ValueError("chat_id is missing")
 
-            event_type = EventService._detect_event_type(message, update)
+            event_type = EventService._detect_event_type(message, event)
 
-            event = Event(event_type=event_type, chat_id=chat_id)
-            return event.model_dump()
+            event_payload = Event(event_type=event_type, chat_id=chat_id)
+            return event_payload.model_dump()
 
         except Exception as e:
             logger.warning(f"Invalid update format: {e}, update={update}")
             return {"msg": "Internal event", "error": str(e)}
 
     @staticmethod
-    def _detect_event_type(message: dict, update: dict) -> EventType:
-        if "new_chat_member" in message:
-            return EventType.NEW_MEMBER
-        if "left_chat_member" in message:
-            return EventType.LEFT_MEMBER
-        if "photo" in message:
-            return EventType.PHOTO
-        return EventType(update.get("event_type", EventType.UNKNOWN))
+    def _detect_event_type(message: dict, event: str) -> str:
+        try:
+            if "new_chat_member" in message:
+                event = "new_chat_member"
+            elif "left_chat_member" in message:
+                event = "left_chat_member"
+
+            return event
+        except Exception as e:
+            logger.warning(f"Failed to detect event type: {e}, message={message}, event={event}")
+            return "unknown"
