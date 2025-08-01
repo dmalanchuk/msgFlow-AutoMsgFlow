9426758340c5003f0be9991bfc2a0427c13fd136
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-08-01 14:02:28 +0300

    rebase method is_event_matched from condition on event services

--- workflow_service/src/services/pattern/condition_service.py
+++ workflow_service/src/services/pattern/condition_service.py
@@ -8,28 +8,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 class ConditionService:
 
     @staticmethod
-    async def is_event_matched(chat_id: int, session: AsyncSession) -> bool:
-        result = await ScenarioRepo.get_scenario(chat_id, session)
-        event = await EventService.check_event(chat_id)
+    async def check_conditions(
+            chat_id: int,
+            redis_service: ServiceRedis,
+            event_service: EventService,
+            session: AsyncSession
+    ) -> bool:
+        """in params used dependency injection"""
 
-        if not event:
-            return False
-
-        incoming_event_type = event.get("event_type")
-        if not incoming_event_type:
-            return False
-
-        for scenario in result:
-            if scenario.event["type"] == incoming_event_type:
-                return True
-
-        return False
-
-    @staticmethod
-    async def check_conditions(chat_id: int, session: AsyncSession) -> bool:
         conditions = await ScenarioRepo.get_scenario(chat_id, session)
-        result = await ConditionService.is_event_matched(chat_id, session)
-        text = await ServiceRedis.get_last_messages(chat_id)
+        result = await event_service.is_event_matched(chat_id, session)
+        text = await redis_service.get_last_messages(chat_id)
 
         last_message = text[0]
 
--- workflow_service/src/services/pattern/event_service.py
+++ workflow_service/src/services/pattern/event_service.py
@@ -1,15 +1,21 @@
 from src.services.redis_service import ServiceRedis
+from src.repositories.scenario_repo import ScenarioRepo
 from src.logger import logger
 
 from src.schemas.event_schema import Event
+
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class EventService:
 
     @staticmethod
-    async def check_event(chat_id: int) -> dict:
+    async def check_event(
+            chat_id: int,
+            redis_service: ServiceRedis
+    ):
         try:
-            updates = await ServiceRedis.get_last_updates(chat_id, limit=1)
+            updates = await redis_service.get_last_updates(chat_id, limit=1)
 
             if not updates:
                 logger.info(f"No updates found for chat: {chat_id}")
@@ -22,7 +28,7 @@ class EventService:
             return {"msg": "Internal error", "error": str(e)}
 
     @staticmethod
-    def _parse_update(update: dict) -> dict:
+    def _parse_update(update: dict):
         try:
             event = list(update)[1]
             message = update.get("message") or update.get("edited_message") or {}
@@ -45,11 +51,41 @@ class EventService:
     def _detect_event_type(message: dict, event: str) -> str:
         try:
             if "new_chat_member" in message:
-                event = "new_chat_member"
+                return "new_chat_member"
             elif "left_chat_member" in message:
-                event = "left_chat_member"
+                return "left_chat_member"
 
             return event
         except Exception as e:
             logger.warning(f"Failed to detect event type: {e}, message={message}, event={event}")
             return "unknown"
+
+    @staticmethod
+    async def is_event_matched(
+            chat_id: int,
+            redis_service: ServiceRedis,
+            scenario_repo: ScenarioRepo,
+            session: AsyncSession
+    ):
+        """in params used dependency injection (DI)"""
+        try:
+
+            scenario_list = await scenario_repo.get_scenario(chat_id, session)
+            event = await EventService.check_event(chat_id, redis_service)
+
+            if not event:
+                return False
+
+            incoming_event_type = event.get("event_type")
+            if not incoming_event_type:
+                return False
+
+            for scenario in scenario_list:
+                if scenario.event["type"] == incoming_event_type:
+                    return True
+
+            return False
+
+        except Exception as e:
+            logger.exception(f"Failed to check event for chat_id={chat_id}, error: {e}")
+            return False
