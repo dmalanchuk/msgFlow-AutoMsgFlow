a942ad03b3ec5269e8c1a5b417a30053f2f14b11
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-08-03 01:58:45 +0300

    added two new depends

--- workflow_service/src/dependency.py
+++ workflow_service/src/dependency.py
@@ -3,10 +3,14 @@ from src.services.scenario_service import ScenarioService
 from src.services.pattern.event_service import EventService
 from src.services.pattern.condition_service import ConditionService
 from src.services.redis_service import ServiceRedis
+from src.services.get_chat_id_service import GetChatIdService
+from src.services.scenario_get_email_service import ScenarioGetEmailService
 
 # class instance - because services used DI (dependency injection)
+get_chat_id_service = GetChatIdService()
+get_email_service = ScenarioGetEmailService()
 redis_service = ServiceRedis()
 scenario_repo = ScenarioRepo()
-scenario_service = ScenarioService(scenario_repo, redis_service)
+scenario_service = ScenarioService(scenario_repo, redis_service, get_email_service, get_chat_id_service)
 event_service = EventService(redis_service, scenario_repo, scenario_service)
 condition_service = ConditionService(redis_service, event_service, scenario_repo, scenario_service)
