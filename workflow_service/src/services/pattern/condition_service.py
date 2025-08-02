7d570380da61cf1d4b35e5c2685f6ae82792c0b6
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-08-03 02:34:13 +0300

    added logger info for result func is_event_matched

--- workflow_service/src/services/pattern/condition_service.py
+++ workflow_service/src/services/pattern/condition_service.py
@@ -1,3 +1,5 @@
+from pygments.lexer import words
+
 from src.repositories.scenario_repo import ScenarioRepo
 from src.services.scenario_service import ScenarioService
 from src.services.pattern.event_service import EventService
@@ -29,19 +31,27 @@ class ConditionService:
         """in params used dependency injection"""
 
         result = await self.event_service.is_event_matched(chat_id, session)
+
+        if not result:
+            logger.info(f"Event not matched for chat_id={chat_id}")
+            return False
+
         text = await self.redis_service.get_last_messages(chat_id)
 
         if not text:
             logger.warning(f"No messages found in Redis for chat_id={chat_id}")
             return False
-
         last_message = text[0]
 
         conditions = await self.scenario_service.get_scenarios(chat_id, session)
 
-        if result:
+        if result and last_message.get("text"):
             for condition in conditions:
-                if condition.get("conditions").get("params")["word"] in last_message.get("text"):
+                conditions_dict = condition.get("conditions") or {}
+                params = conditions_dict.get("params") or {}
+                word = params.get("word")
+
+                if word and word in last_message["text"]:
                     return True
 
         return False
