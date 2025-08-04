1bbae5e244698ea9e66c6ce05e6109c5874ad397
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-08-04 16:54:58 +0300

    end write logger in auth

--- auth_service/src/services/get_profile_service.py
+++ auth_service/src/services/get_profile_service.py
@@ -4,17 +4,18 @@ from starlette.requests import Request
 from src.repositories.user_repo import UserRepo
 from src.models.users_model import UserModel
 
+from src.logger import logger
+
 
 class GetProfileService:
     @staticmethod
     async def get_profile_service(request: Request, session: AsyncSession):
 
         user_email = request.headers.get("X-User-Email")
-        print("🧾 Отриманий X-User-Email:", user_email)
+        logger.debug(f"X-User-Email: {user_email}")
         if not user_email:
             raise HTTPException(status_code=400, detail="Missing user email in request state")
 
-        # Знайти користувача по email
         user: UserModel | None = await UserRepo.get_by_email(session, user_email)
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
