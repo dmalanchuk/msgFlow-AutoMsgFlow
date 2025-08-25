7ec1216ee3e7fa3efe558246e82f42076038c969
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-08-26 00:22:11 +0300

    started wrote new util

--- workflow_service/src/utils/get_current_user.py
+++ workflow_service/src/utils/get_current_user.py
@@ -3,6 +3,14 @@ from datetime import datetime
 import jwt
 from jwt import PyJWTError
 
+from src.schemas.token_schema import Token
 from src.config import settings
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://localhost:8001/auth/login")
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)) -> Token:
+    ...
