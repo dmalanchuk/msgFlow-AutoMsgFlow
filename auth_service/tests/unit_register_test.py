71a90e7d768c3d9e7a7d92c385e78368e8492995
M1Hato <miro28795@gmail.com>
2025-08-30 02:48:37 +0300

    create new func test reg user

---
+++ auth_service/tests/unit_register_test.py
@@ -0,0 +1,23 @@
+from unittest.mock import AsyncMock, patch
+
+import pytest
+
+import src.services.reg_service
+from src.schemas.user_schema import CreateUser
+from src.services.reg_service import RegServices
+
+
+@pytest.mark.asyncio
+async def test_register_user_success():
+    fake_user = CreateUser(email="mini@gmail.com", username="mini", password="111qwe111")
+    fake_session = AsyncMock()
+
+    with patch("src.services.reg_service.RegUser.get_by_email", new=AsyncMock(return_value=None)):
+        with patch("src.services.reg_service.RegUser.create_user", new=AsyncMock(return_value={"id": 1, "username": fake_user.username, "email": fake_user.email})):
+            user = await RegServices.reg_user_service(fake_user, fake_session)
+
+    assert user["id"] == 1
+    assert user["username"] == fake_user.username
+    assert user["email"] == fake_user.email
+
+
