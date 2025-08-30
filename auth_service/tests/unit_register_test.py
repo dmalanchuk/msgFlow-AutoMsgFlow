c40189efdf229e7f70ba7230f6825d92da40afb9
M1Hato <miro28795@gmail.com>
2025-08-30 03:01:49 +0300

    create new func test reg user for already exists user

--- auth_service/tests/unit_register_test.py
+++ auth_service/tests/unit_register_test.py
@@ -1,8 +1,8 @@
 from unittest.mock import AsyncMock, patch
 
 import pytest
+from fastapi import HTTPException
 
-import src.services.reg_service
 from src.schemas.user_schema import CreateUser
 from src.services.reg_service import RegServices
 
@@ -19,5 +19,17 @@ async def test_register_user_success():
     assert user["id"] == 1
     assert user["username"] == fake_user.username
     assert user["email"] == fake_user.email
+
+
+@pytest.mark.asyncio
+async def test_reg_user_already_exists():
+    fake_user = CreateUser(email="mini@gmail.com", username="mini", password="111qwe111")
+    fake_session = AsyncMock()
+
+    with patch("src.services.reg_service.RegUser.get_by_email", new=AsyncMock(return_value={"id": 1, "username": fake_user.username, "email": fake_user.email})):
+        with pytest.raises(HTTPException) as exception:
+            await RegServices.reg_user_service(fake_user, fake_session)
+
+    assert exception.value.status_code == 409
 
 
