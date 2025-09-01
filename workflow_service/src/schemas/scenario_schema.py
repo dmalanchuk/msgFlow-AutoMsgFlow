6b67cf96349d4ad557a3f9a20bd6a114e163141f
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-09-01 14:41:26 +0300

    update scenario schema added letter_match_pattern to check if the name consists of only letters and added new schema for Condition/Action params

--- workflow_service/src/schemas/scenario_schema.py
+++ workflow_service/src/schemas/scenario_schema.py
@@ -1,60 +1,43 @@
-from typing import Dict, Any, Literal, Optional
-from pydantic import BaseModel, EmailStr
+from typing import Any, Literal, Optional
+from pydantic import BaseModel, EmailStr, Field
+import re
 
-"""
-    Scenario schema haver the same structure as in the database,
-    and are used three unique classes for validation:
-        - EventInfo
-        - Condition
-        - Action 
-        
-    This classes are used 'Literal' for validation, 
-    and are used in the ScenarioCreate class for validation.  
-"""
+LETTER_MATCH_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z]+$")
+
+""""""
 
 
-# class EventInfo(BaseModel):
-#     type: Literal["message_received", "user_joined", "user_left", "new_post"]
-#     source: Literal["telegram", "discord", "email"]  # add checking if email, add new column where u write your email
-#
-#
-# class Condition(BaseModel):
-#     type: Literal["contains_word", "equals"]
-#     params: Dict[str, Any]
-#
-#
-# class Action(BaseModel):
-#     type: Literal["send_message", "forward"]
-#     params: Dict[str, Any]
-#
-#
-# class ScenarioCreate(BaseModel):
-#     name: str
-#     chat_url: str
-#     owner_email: str
-#     event: EventInfo
-#     condition: Condition
-#     action: Action
-#
-#     class Config:
-#         from_attributes = True
-
-
-# scenario patch update
-
-
-# class ScenarioPatchUpdate(BaseModel):
-#     name: Optional[str] = None
-#     chat_url: Optional[str] = None
-#     event: Optional[EventInfo] = None
-#     conditions: Optional[Condition] = None
-#     actions: Optional[Action] = None
-
-
-# ============scenario schemas v2============
+class ParamsContainsWord(BaseModel):
+    word: str = Field(..., min_length=1, description="Key word for executing action")
 
 
 class ScenarioCreate(BaseModel):
-    name: str
+    name: str = Field(..., min_length=1,  max_length=16)
     chat_url: str
     owner_email: EmailStr
+
+    class Config:
+        from_attributes = True
+
+
+class EventCreate(BaseModel):
+    type: Literal["message_received", "user_joined", "user_left", "new_post"]
+    source: Literal["telegram"]
+
+
+class ConditionCreate(BaseModel):
+    type: Literal["contains_word"]
+    params: ParamsContainsWord
+
+
+class ActionCreate(BaseModel):
+    type: Literal["send_message", "forward"]
+    params: dict[str, Any]
+
+
+class ScenarioUpdate(BaseModel):
+    name: Optional[str] = None
+    chat_url: Optional[str] = None
+    event: Optional[list[EventCreate]] = None
+    conditions: Optional[list[ConditionCreate]] = None
+    actions: Optional[list[ActionCreate]] = None
