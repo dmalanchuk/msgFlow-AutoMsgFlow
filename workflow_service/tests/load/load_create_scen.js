3f5c5ff40ea1641dd55259a78db483a50da210ec
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-09-02 16:45:49 +0300

    executing test 500vus: test completed 100%

--- workflow_service/tests/load/load_create_scen.js
+++ workflow_service/tests/load/load_create_scen.js
@@ -3,34 +3,48 @@ import { check, sleep } from "k6";
 
 // Налаштування навантаження
 export const options = {
-  vus: 80,         // кількість віртуальних користувачів
+  vus: 500,         // кількість віртуальних користувачів
   duration: "30s", // час тесту
 };
 
+// Список дозволених доменів для owner_email
+const DOMAINS = ["gmail.com", "ukr.net"];
+
+function randomEmail(uid) {
+  const domain = DOMAINS[Math.floor(Math.random() * DOMAINS.length)];
+  return `user${uid}@${domain}`;
+}
+
 export default function () {
-  // Робимо унікальність через timestamp + випадкове число
+  // Унікальний ідентифікатор для генерації унікальних даних
   const uid = `${Date.now()}-${Math.floor(Math.random() * 100000)}`;
 
   const payload = JSON.stringify({
-    name: `scenario-${uid}`,
+    name: `scenario`,
     chat_url: "https://t.me/testingmsgflow",
-    owner_email: "example@gmail.com",
-    event: {
-      type: "message_received",
-      source: "telegram",
-    },
-    condition: {
-      type: "contains_word",
-      params: {
-        word: `word-${uid}`,
+    owner_email: randomEmail(uid),
+    event: [
+      {
+        type: "message_received",
+        source: "telegram",
       },
-    },
-    action: {
-      type: "send_message",
-      params: {
-        text: `new post ${uid}`,
+    ],
+    conditions: [
+      {
+        type: "contains_word",
+        params: {
+          word: `word-${uid}`,
+        },
       },
-    },
+    ],
+    actions: [
+      {
+        type: "send_message",
+        params: {
+          text: `new post ${uid}`,
+        },
+      },
+    ],
   });
 
   const headers = {
@@ -38,7 +52,8 @@ export default function () {
     Accept: "application/json",
   };
 
-  const res = http.post("http://127.0.0.1:8003/scenarios", payload, {
+  // Відправка POST-запиту
+  const res = http.post("http://127.0.0.1:8003/scenarios/scenario", payload, {
     headers,
   });
 
