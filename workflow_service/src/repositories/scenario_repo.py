4ddd53616415592fe870482694b280b21ba0f5fa
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-09-03 22:12:58 +0300

    Converted from class style to functional

--- workflow_service/src/repositories/scenario_repo.py
+++ workflow_service/src/repositories/scenario_repo.py
@@ -4,52 +4,49 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, and_, delete, update
 
 
-class ScenarioRepo:
+async def get_scenario_repo(email: str, session: AsyncSession):
+    result = await session.execute(
+        select(ScenariosModel).where(ScenariosModel.owner_email == email)
+    )
+    return result.scalars().all()
 
-    @staticmethod
-    async def get_scenario(email: str, session: AsyncSession):
-        result = await session.execute(
-            select(ScenariosModel).where(ScenariosModel.owner_email == email)
-        )
-        return result.scalars().all()
 
-    @staticmethod
-    async def get_scenarios(chat_id: int, session: AsyncSession):
-        result = await session.execute(
-            select(ScenariosModel).where(ScenariosModel.chat_id == chat_id)
-        )
-        return result.scalars().all()
+async def get_scenarios(chat_id: int, session: AsyncSession):
+    result = await session.execute(
+        select(ScenariosModel).where(ScenariosModel.chat_id == chat_id)
+    )
+    return result.scalars().all()
 
-    @staticmethod
-    async def create_scenario(session: AsyncSession, scenarios: ScenariosModel):
-        session.add(scenarios)
-        await session.commit()
-        await session.refresh(scenarios)
-        return scenarios
 
-    @staticmethod
-    async def del_by_name(name: str, owner_email: str, session: AsyncSession):
-        result = await session.execute(
-            delete(ScenariosModel)
-            .where(
-                and_(
-                    ScenariosModel.name == name,
-                    ScenariosModel.owner_email == owner_email
-                )
+async def create_scenario_repo(session: AsyncSession, scenarios: ScenariosModel):
+    session.add(scenarios)
+    await session.commit()
+    await session.refresh(scenarios)
+    return scenarios
+
+
+async def del_by_name(name: str, owner_email: str, session: AsyncSession):
+    result = await session.execute(
+        delete(ScenariosModel)
+        .where(
+            and_(
+                ScenariosModel.name == name,
+                ScenariosModel.owner_email == owner_email
             )
-            .returning(ScenariosModel.id)
         )
-        return result.scalar_one_or_none()
+        .returning(ScenariosModel.id)
+    )
+    return result.scalar_one_or_none()
 
-    @staticmethod
-    async def get_by_name_email(name: str, email: str, values: dict, session: AsyncSession):
-        query = await session.execute(
-            update(ScenariosModel).values(**values).where(
-                and_(
-                    ScenariosModel.name == name,
-                    ScenariosModel.owner_email == email
-                )
+
+async def get_by_name_email(name: str, email: str, values: dict, session: AsyncSession):
+    query = await session.execute(
+        update(ScenariosModel).values(**values).where(
+            and_(
+                ScenariosModel.name == name,
+                ScenariosModel.owner_email == email
             )
-            .returning(ScenariosModel)
         )
-        return query.scalars().one_or_none()
+        .returning(ScenariosModel)
+    )
+    return query.scalars().one_or_none()
--- workflow_service/src/services/scenario_service.py
+++ workflow_service/src/services/scenario_service.py
@@ -3,11 +3,15 @@ import json
 from fastapi import Request, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.redis.redis_service import ServiceRedis
+from src.redis.redis_service import (
+    set_raw, get_raw,
+)
 from src.models.scenarios_model import ScenariosModel, EventsModel, ConditionsModel, ActionsModel
 from src.schemas.scenario_schema import ScenarioCreate, ScenarioUpdate
+from src.repositories.scenario_repo import (
+    create_scenario_repo, del_by_name, get_by_name_email, get_scenarios
+)
 
-from src.repositories.scenario_repo import ScenarioRepo
 from src.utils.get_chat_id import get_chat_id
 from src.utils.get_user_email import get_user_email
 
@@ -17,98 +21,89 @@ from src.utils.get_user_email import get_user_email
 """
 
 
-class ScenarioService:
-    def __init__(
-            self,
-            scenarios_repo: ScenarioRepo,
-            redis_service: ServiceRedis,
-    ):
-        self.scenarios_repo = scenarios_repo
-        self.redis_service = redis_service
+# create scenarios
+async def create_scenario_service(
+        session: AsyncSession,
+        scenario: ScenarioCreate,
+        request: Request
+):
+    try:
+        chat_id = await get_chat_id(scenario.chat_url)
+        user_email = scenario.owner_email if scenario.owner_email else get_user_email(request)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-    # create scenarios
-    async def create_scenario(
-            self,
-            session: AsyncSession,
-            scenario: ScenarioCreate,
-            request: Request
-    ):
+    new_scenario = ScenariosModel(
+        name=scenario.name,
+        owner_email=user_email,
+        chat_id=chat_id,
+        events=[
+            EventsModel(**event.model_dump())
+            for event in scenario.event
+        ],
+        conditions=[
+            ConditionsModel(**conditions.model_dump())
+            for conditions in scenario.conditions
+        ],
+        actions=[
+            ActionsModel(**actions.model_dump())
+            for actions in scenario.actions
+        ]
+    )
 
-        try:
-            chat_id = await get_chat_id(scenario.chat_url)
-            user_email = scenario.owner_email if scenario.owner_email else get_user_email(request)
-        except ValueError as e:
-            raise HTTPException(status_code=400, detail=str(e))
+    return await create_scenario_repo(session, new_scenario)
 
-        new_scenario = ScenariosModel(
-            name=scenario.name,
-            owner_email=user_email,
-            chat_id=chat_id,
-            events=[
-                EventsModel(**event.model_dump())
-                for event in scenario.event
-            ],
-            conditions=[
-                ConditionsModel(**conditions.model_dump())
-                for conditions in scenario.conditions
-            ],
-            actions=[
-                ActionsModel(**actions.model_dump())
-                for actions in scenario.actions
-            ]
-        )
 
-        return await self.scenarios_repo.create_scenario(session, new_scenario)
+# needs refactoring: get scenarios
+async def get_scenarios_service(
+        chat_id: int,
+        session: AsyncSession,
+        ttl: int = 1000
+):
+    key = f"chat:{chat_id}:scenarios"
 
-    # needs refactoring: get scenarios
-    async def get_scenarios(
-            self,
-            chat_id: int,
-            session: AsyncSession,
-            ttl: int = 1000
-    ):
-        key = f"chat:{chat_id}:scenarios"
+    cached = await get_raw(key)
+    if cached:
+        return json.loads(cached)
 
-        cached = await self.redis_service.get_raw(key)
-        if cached:
-            return json.loads(cached)
+    scenarios = await get_scenarios(chat_id, session)
 
-        scenarios = await self.scenarios_repo.get_scenarios(chat_id, session)
+    to_cache = []
+    for scenario in scenarios:
+        to_cache.append({
+            "chat_id": scenario.chat_id,
+            "event": scenario.event,
+            "conditions": scenario.conditions,
+            "actions": scenario.actions
+        })
 
-        to_cache = []
-        for scenario in scenarios:
-            to_cache.append({
-                "chat_id": scenario.chat_id,
-                "event": scenario.event,
-                "conditions": scenario.conditions,
-                "actions": scenario.actions
-            })
+    await set_raw(key, json.dumps(to_cache), ttl)
+    return to_cache
 
-        await self.redis_service.set_raw(key, json.dumps(to_cache), ttl)
-        return to_cache
 
-    # delete scenarios by name
-    async def delete_scenario(self, name: str, owner_email: str, session: AsyncSession):
-        async with session.begin():
-            scenario = await self.scenarios_repo.del_by_name(name, owner_email, session)
+# delete scenarios by name
+async def delete_scenario_service(name: str, owner_email: str, session: AsyncSession):
+    async with session.begin():
+        scenario = await del_by_name(name, owner_email, session)
 
-            if not scenario:
-                raise HTTPException(status_code=404, detail="Scenario not found")
-        return {"msg": "Scenario deleted"}
+        if not scenario:
+            raise HTTPException(status_code=404, detail="Scenario not found")
+    return {"msg": "Scenario deleted"}
 
-    # update scenario
-    async def update_scenario_patch(self, name: str, owner_email: str, body: ScenarioUpdate,
-                                    session: AsyncSession):
-        async with session.begin():
-            body = body.model_dump(exclude_none=True)
 
-            if not body:
-                raise HTTPException(
-                    status_code=400, detail="Invalid request. Body cannot be empty"
-                )
+# update scenario
+async def update_scenario_patch(name: str, owner_email: str, body: ScenarioUpdate,
+                                session: AsyncSession):
+    async with session.begin():
+        body = body.model_dump(exclude_none=True)
 
-            scenario = await self.scenarios_repo.get_by_name_email(name, owner_email, body, session)
-            if not scenario:
-                raise HTTPException(status_code=404, detail="Scenario not found")
+        if not body:
+            raise HTTPException(
+                status_code=400, detail="Invalid request. Body cannot be empty"
+            )
 
-        return {"msg": "Scenario updated"}
+        scenario = await get_by_name_email(name, owner_email, body, session)
+        if not scenario:
+            raise HTTPException(status_code=404, detail="Scenario not found")
+
+    return {"msg": "Scenario updated"}
