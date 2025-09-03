8db2786ef5f5c1622cc147ebc2e8dbdfe65458ee
M1Hato <miro28795@gmail.com>
2025-09-03 12:29:38 +0300

    removed call from RegUser - get_by_email

--- auth_service/src/services/reg_service.py
+++ auth_service/src/services/reg_service.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException
+from pydantic.v1 import IntegerError
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -12,10 +13,7 @@ class RegServices:
 
     @staticmethod
     async def reg_user_service(data: CreateUser, session: AsyncSession):
-        new_user = await RegUser.get_by_email(data.email, session)
-        if new_user:
-            raise HTTPException(status_code=409, detail="User is already registered!!")
-
         data.password = hash_password(data.password)
         new_user_model = UserModel(**data.model_dump())
+
         return await RegUser.create_user(new_user_model, session)
