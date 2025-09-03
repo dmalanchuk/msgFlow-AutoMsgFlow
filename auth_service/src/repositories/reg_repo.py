10540be87d68678fb6d5d8a85a8fde335d6f0c12
M1Hato <miro28795@gmail.com>
2025-09-03 12:28:39 +0300

    removed the method get_by_email in reg_repo for optimization, and add try-except fot create_user

--- auth_service/src/repositories/reg_repo.py
+++ auth_service/src/repositories/reg_repo.py
@@ -1,5 +1,5 @@
-from pydantic import EmailStr
-from sqlalchemy import select
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.models.users_model import UserModel
@@ -8,16 +8,11 @@ from src.models.users_model import UserModel
 class RegUser:
 
     @staticmethod
-    async def get_by_email(email: EmailStr, session: AsyncSession):
-        result = await session.execute(
-            select(UserModel).where(UserModel.email == email)
-        )
-        return result.scalars().first()
-
-    @staticmethod
     async def create_user(user: UserModel, session: AsyncSession):
         session.add(user)
-        await session.commit()
-        await session.refresh(user)
-
-        return user
+        try:
+            await session.commit()
+            await session.refresh(user)
+        except IntegrityError:
+            await session.rollback()
+            raise HTTPException(status_code=409, detail="User is already registered!!")
