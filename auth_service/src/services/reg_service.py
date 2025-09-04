be5af3792136c44b9ab72f14da0e565c4914b070
M1Hato <miro28795@gmail.com>
2025-09-04 03:11:06 +0300

    added await for hash password

--- auth_service/src/services/reg_service.py
+++ auth_service/src/services/reg_service.py
@@ -1,8 +1,4 @@
-from fastapi import HTTPException
-from pydantic.v1 import IntegerError
-
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from src.models.users_model import UserModel
 from src.schemas.user_schema import CreateUser
 from src.repositories.reg_repo import RegUser
@@ -13,7 +9,7 @@ class RegServices:
 
     @staticmethod
     async def reg_user_service(data: CreateUser, session: AsyncSession):
-        data.password = hash_password(data.password)
+        data.password = await hash_password(data.password)
         new_user_model = UserModel(**data.model_dump())
 
         return await RegUser.create_user(new_user_model, session)
