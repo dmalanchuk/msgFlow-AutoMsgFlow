1989223c337a12cea510cf279bad921d04603b64
M1Hato <miro28795@gmail.com>
2025-09-04 03:10:39 +0300

    return func get_by_email for login and return value user for register

--- auth_service/src/repositories/reg_repo.py
+++ auth_service/src/repositories/reg_repo.py
@@ -1,11 +1,20 @@
-from fastapi import HTTPException
-from sqlalchemy.exc import IntegrityError
+from pydantic import EmailStr
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.models.users_model import UserModel
 
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
 
 class RegUser:
+
+    @staticmethod
+    async def get_by_email(email: EmailStr, session: AsyncSession):
+        result = await session.execute(
+            select(UserModel).where(UserModel.email == email)
+        )
+        return result.scalars().first()
 
     @staticmethod
     async def create_user(user: UserModel, session: AsyncSession):
@@ -16,3 +25,4 @@ class RegUser:
         except IntegrityError:
             await session.rollback()
             raise HTTPException(status_code=409, detail="User is already registered!!")
+        return user
