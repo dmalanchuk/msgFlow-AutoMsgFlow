a06e619ff2f784b264f6eaeb80ded1a5cd7fc339
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-09-09 22:34:44 +0300

    create three files: repo, service and schemas for update logic

---
+++ workflow_service/src/repositories/scenario_update_repo.py
@@ -0,0 +1,47 @@
+from pydantic import EmailStr
+
+from src.models.scenarios_model import ScenariosModel, EventsModel
+from src.schemas.scenario_update_schema import UpdateEvent
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import and_, update, select
+
+
+async def update_scenario(name: str, email: EmailStr, values: dict, session: AsyncSession):
+    async with session.begin():
+        query = await session.execute(
+            update(ScenariosModel).values(**values).where(
+                and_(
+                    ScenariosModel.name == name,
+                    ScenariosModel.owner_email == email
+                )
+            )
+        )
+    return query.rowcount
+
+
+async def update_eca(
+        name: str,
+        email: EmailStr,
+        values: dict,
+        session: AsyncSession
+):
+    async with session.begin():
+        scenario = await session.execute(
+            select(ScenariosModel).where(
+                and_(
+                    ScenariosModel.name == name,
+                    ScenariosModel.owner_email == email
+                )
+            )
+        )
+        scenario = scenario.scalars().first()
+        if not scenario:
+            return None
+
+        query = await session.execute(
+            update(EventsModel).values(**values).where(
+                EventsModel.scenario_id == scenario.id
+            )
+        )
+    return query.rowcount
--- workflow_service/src/services/scenario_update_service.py
+++ workflow_service/src/services/scenario_update_service.py
@@ -1,19 +1,46 @@
+from fastapi import HTTPException
 from pydantic import EmailStr
-from src.models.scenarios_model import ScenariosModel
-from src.schemas.scenario_schema import ScenarioUpdate
-
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, and_
+
+from src.repositories.scenario_update_repo import update_scenario, update_eca
+from src.schemas.scenario_update_schema import UpdateScenario, UpdateEvent
 
 
-# needs refactoring
-async def update_by_name(name: str, email: EmailStr, values: dict, session: AsyncSession):
-    query = await session.execute(
-        update(ScenariosModel).values(**values).where(
-            and_(
-                ScenariosModel.name == name,
-                ScenariosModel.owner_email == email
-            )
+async def update_scenario_service(
+        name: str,
+        email: EmailStr,
+        body: UpdateScenario,
+        session: AsyncSession
+):
+    body = body.model_dump(exclude_none=True)
+
+    if not body:
+        raise HTTPException(
+            status_code=400, detail="Invalid request. Body cannot be empty"
         )
-    )
-    return query.scalars().one_or_none()
+
+    scenario = await update_scenario(name, email, body, session)
+    if not scenario:
+        raise HTTPException(status_code=404, detail="Scenario not found")
+
+    return {"msg": "Scenario updated"}
+
+
+async def update_eca_service(
+        name: str,
+        email: EmailStr,
+        body: UpdateEvent,
+        session: AsyncSession
+):
+    body = body.model_dump(exclude_none=True)
+
+    if not body:
+        raise HTTPException(
+            status_code=400, detail="Invalid request. Body cannot be empty"
+        )
+
+    scenario = await update_eca(name, email, body, session)
+    if not scenario:
+        raise HTTPException(status_code=404, detail="Scenario not found")
+
+    return {"msg": "Scenario updated"}
