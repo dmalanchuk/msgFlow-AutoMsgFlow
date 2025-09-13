dc6a222313a793d4440c3f727693fec96e10d16b
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-09-13 17:03:22 +0300

    update redis service, del save message, updated save update

--- workflow_service/src/redis/redis_service.py
+++ workflow_service/src/redis/redis_service.py
@@ -1,25 +1,30 @@
-from src.redis.client_redis import redis
 import json
+from pydantic import EmailStr
+
+from src.redis.client_redis import redis
+from src.schemas.event_schema import UpdateRaw
+from src.utils.make_safe_mail import make_chat_key
 
 
 # saved last updates and messages
-async def save_update(chat_id: int, update: dict):
-    key = f"chat:{chat_id}:updates"
-    await redis.rpush(key, json.dumps(update))
+async def save_update(email: EmailStr, update: UpdateRaw):
+    safe_email = make_chat_key(email)
+    key = f"chat:{safe_email}:updates"
+    await redis.rpush(key, update.model_dump_json())
     await redis.expire(key, 500)  # 86400
 
 
-async def save_message(chat_id: int, text: str, msg_id: int, source: str, event_type: str):
-    key = f"chat:{chat_id}:messages"
-    await redis.rpush(key, json.dumps(
-        {
-            "source": source,
-            "event_type": event_type,
-            "text": text,
-            "message_id": msg_id
-        }
-    ))
-    await redis.expire(key, 500)  # 86400
+# async def save_message(chat_id: int, text: str, msg_id: int, source: str, event_type: str):
+#     key = f"chat:{chat_id}:messages"
+#     await redis.rpush(key, json.dumps(
+#         {
+#             "source": source,
+#             "event_type": event_type,
+#             "text": text,
+#             "message_id": msg_id
+#         }
+#     ))
+#     await redis.expire(key, 500)  # 86400
 
 
 # Save action in Redis
