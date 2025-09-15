afdc7d81cd47ec9a3220b61d3a0846cfd38d261a
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-09-15 21:24:52 +0300

    updated e pattern

--- workflow_service/src/services/pattern/event_service.py
+++ workflow_service/src/services/pattern/event_service.py
@@ -1,30 +1,28 @@
-from pydantic import EmailStr
-
+from src.repositories.scenario_repo import get_scenarios_all_by_chat_id
 from src.logger import logger
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.redis.redis_service import get_last_updates
-from src.repositories.scenario_repo import get_scenarios_all
 
 
 async def check_event(
-        email: EmailStr,
+        chat_id: int,
         session: AsyncSession
 ) -> bool | None:
     try:
-        updates = await get_last_updates(email, limit=1)
+        updates = await get_last_updates(chat_id, limit=1)
 
         if not updates:
-            logger.info(f"No updates found for email: {email}")
+            logger.info(f"No updates found for chat_id: {chat_id}")
             return None
 
-        scenarios = await get_scenarios_all(email, session)
+        scenarios = await get_scenarios_all_by_chat_id(chat_id, session)
         for scenario in scenarios:
-            if scenario.event.event_type == updates[0].event_type:
+            if scenario.events.event_type == updates[0].event_type:
                 return True
 
         return False
 
     except Exception as e:
-        logger.exception(f"Failed to check event for email={email}, error={e}")
+        logger.exception(f"Failed to check event for chat_id={chat_id}, error={e}")
         return False
