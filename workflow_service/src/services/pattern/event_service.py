5326b1468c4b0e85e3876b67b6bf5db49ac7e836
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-09-19 15:23:21 +0300

    refact added last_update convert from updates to dict and event_type getting attribute event_type

--- workflow_service/src/services/pattern/event_service.py
+++ workflow_service/src/services/pattern/event_service.py
@@ -16,10 +16,16 @@ async def check_event(
             logger.info(f"No updates found for chat_id: {chat_id}")
             return None
 
+        last_update = updates[0]
+        event_type = last_update.get("event_type")
+
         scenarios = await get_scenarios_all_by_chat_id(chat_id, session)
+
         for scenario in scenarios:
-            if scenario.events.event_type == updates[0].event_type:
-                return True
+            for event in scenario.events:
+                if event.type == event_type:
+                    logger.info(f"Matched event for chat_id={chat_id}, event_type={last_update.event_type}")
+                    return True
 
         return False
 
