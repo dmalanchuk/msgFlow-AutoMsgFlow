4a249d135282c99579bb463521dae0dfa81cc56d
Daniil Malanchuck <124836521+dmalanchuk@users.noreply.github.com>
2025-10-10 21:31:24 +0300

    commited all file

--- workflow_service/src/dependency.py
+++ workflow_service/src/dependency.py
@@ -1,13 +1,13 @@
-
-from src.services.pattern.event_service import EventService
-from src.services.pattern.condition_service import ConditionService
-from src.services.pattern.action_service import ActionService
-
-
+#
+# from pattern.event_service import EventService
+# from pattern.condition_service import ConditionService
+# from pattern.action_service import ActionService
+#
+#
 # class instance - because services used DI (dependency injection)
-
-
+#
+#
 # patterns
-event_service = EventService()
-condition_service = ConditionService(event_service)
-action_service = ActionService(condition_service)
+# event_service = EventService()
+# condition_service = ConditionService(event_service)
+# action_service = ActionService(condition_service)
